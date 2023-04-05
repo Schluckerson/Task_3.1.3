6bbe22ea8f240aac79954da4dc2933c1a5fdbad7
Schluckerson <schluckerson@gmail.com>
2023-04-05 19:57:00 +0400

    1

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -2,7 +2,6 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import ru.kata.spring.boot_security.demo.service.UserService;
